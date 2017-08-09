8816b9ed95adefdc59b790e7ead34a8ba8ae38dd
Eric-Kosovec <ej.kosovec@gmail.com>
2017-08-09 02:33:47 -0500

    Added a user input dictionary.

--- src/DougBot.py
+++ src/DougBot.py
@@ -6,6 +6,7 @@ from DougBotProperties import *
 
 client = commands.Bot(description=DESCRIPTION, command_prefix=COMMAND_PREFIX)
 
+user_dict = dict()
 
 class DougBot(discord.Client):
     def __init__(self):
@@ -53,5 +54,54 @@ async def slap(ctx, args):
 async def github(ctx):
     await client.say(GITHUB)
 
+
+@client.command(pass_context=True)
+async def putdict(ctx, args):
+    strs = args.split(":")
+    key = strs[0]
+    value = strs[1]
+    user_dict[key] = value
+    await client.say("{} : {} inserted into dictionary.".format(key, value))
+
+
+@client.command(pass_context=True)
+async def querydict(ctx, args):
+    if args in user_dict:
+        await client.say("{} found in dictionary.".format(args))
+    else:
+        await client.say("{} not found in dictionary.".format(args))
+
+
+@client.command(pass_context=True)
+async def removedict(ctx, args):
+    try:
+        value = user_dict[args]
+        del user_dict[args]
+        await client.say("{} : {} not removed from dictionary.".format(args, value))
+    except KeyError:
+        await client.say("{} not found in dictionary.".format(args))
+
+
+@client.command(pass_context=True)
+async def cleardict(ctx):
+    user_dict.clear()
+    await client.say("Dictionary cleared.")
+
+
+@client.command(pass_context=True)
+async def dictsize(ctx):
+    await client.say("{}".format(len(user_dict)))
+
+
+@client.command(pass_context=True)
+async def printdict(ctx):
+    dict_str = "["
+
+    for (key, value) in user_dict.items():
+        dict_str += " " + key + ":" + value
+
+    dict_str += " ]"
+
+    await client.say(dict_str)
 
 client.run(TOKEN)
