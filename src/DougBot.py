602ecad658320675733b19cee6ec88ad198b18f8
Eric-Kosovec <ej.kosovec@gmail.com>
2017-08-09 01:44:52 -0500

    Added probably working commands join and leave.

--- src/DougBot.py
+++ src/DougBot.py
@@ -1,7 +1,8 @@
 import discord
-from DougBotProperties import *
-from Logger import *
+from discord.channel import *
 from discord.ext import commands
+
+from DougBotProperties import *
 
 client = commands.Bot(description=DESCRIPTION, command_prefix=COMMAND_PREFIX)
 
@@ -15,7 +16,7 @@ class DougBot(discord.Client):
 
 @client.event
 async def on_ready():
-    init_logger()
+    # init_logger()
 
     print("Bot online")
     print("Name: {}".format(client.user.name))
@@ -23,10 +24,24 @@ async def on_ready():
     print("-----------------------")
 
 
-
 @client.command(pass_context=True)
 async def ping(ctx):
     await client.say("pong")
+
+
+@client.command(pass_context=True)
+async def join(ctx):
+    channel = discord.utils.get(client.get_all_channels(), name='SadDoug Central', type=ChannelType.voice)
+    client.join_voice_channel(channel)
+
+
+@client.command(pass_context=True)
+async def leave(ctx):
+    channel = discord.utils.get(client.get_all_channels(), name='SadDoug Central', type=ChannelType.voice)
+    voice_client = client.voice_client_in(channel.server)
+    if voice_client is not None:
+        await voice_client.disconnect()
+    return
 
 
 @client.command(pass_context=True)
