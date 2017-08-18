0185ef59f45ca04e132a3ef3235b3f81f9f46110
Eric-Kosovec <ej.kosovec@gmail.com>
2017-08-18 18:39:50 -0500

    Cleaned up and optimized code.

--- dougbot/dougbot.py
+++ dougbot/dougbot.py
@@ -1,44 +1,57 @@
+import asyncio
 import os
+import socket
 import sys
 
+import aiohttp
 import discord.client
 from discord import Message
 
-from configdata import Config
+from config import Config
+from exceptions.commanderror import *
+
+
+# TODO LOGGING
 
 
 class DougBot(discord.Client):
-    _DEFAULT_CONFIG_FILE = "./config/config.ini"
+    # TODO DECIDE HOW TO DO SO IT WORKS WHILE RUNNING FROM DOUGBOT.PY AND RUN.PY
+    _DEFAULT_CONFIG_FILE = "../config/config.ini"
+    _QUESTION_EMOJI = "❓"  # The Unicode string of the question emoji.
 
     def __init__(self, config_file=_DEFAULT_CONFIG_FILE):
         self.config = Config(config_file)
-        self.plugins = {}
-        self._load_plugins()
+        self.plugins = self._load_plugins()
+        # self.logger = get_logger()
         super().__init__()
-        return
 
     def run(self):
         try:
             self.loop.run_until_complete(self.start(self.config.token))
         except discord.errors.LoginFailure:
             print("Bot could not login. Bad token.")
+        except socket.gaierror:
+            print("Bot could not login. Could not connect to Discord servers.")
+        except aiohttp.errors.ClientOSError:
+            print("Bot could not login. Could not connect to Discord servers.")
         finally:
-            # TODO CLEANUP
+            try:
+                self._cleanup()
+            except Exception as e:
+                print("Cleanup error: %s", e)
             self.loop.close()
 
     async def on_ready(self):
-        # init_logger()
-
         print("Bot online")
         print("Name: %s" % self.user.name)
         print("ID: %s" % self.user.id)
-        print("-----------------------")
+        print("-" * (len(self.user.id) + 4))
 
     async def on_message(self, message: Message):
         # Wait until the bot is ready before checking messages.
         await self.wait_until_ready()
 
-        # Avoid any sort of bot talking to bot type situation.
+        # Avoid any sort of bot talking to bot-type situation.
         if message.author.id == self.user.id or message.author.bot:
             return
 
@@ -52,44 +65,81 @@ class DougBot(discord.Client):
 
         try:
             plugin = self.plugins[command]
-        except KeyError as e:
-            await self.send_message(message.channel, "%s is not a command." % command)
-            return
 
-        if plugin is None:
-            await self.send_message(message.channel, "Command %s is not currently working." % command)
-        else:
-            try:
+            if plugin is None:
+                await self.add_reaction(message, self._QUESTION_EMOJI)
+            else:
                 await plugin.run(command, message, arguments, self)
-            except Exception as e:
-                await self.send_message(message.channel, "Command %s is not currently working." % command)
-        return
+        except KeyError as e:  # Command not in dictionary.
+            await self.add_reaction(message, self._QUESTION_EMOJI)
+        except Exception as e:  # Catch any other errors that may occur.
+            await self.add_reaction(message, self._QUESTION_EMOJI)
+            print("Error occurred while running command: %s" % e)
 
-    def _load_plugins(self):
+    def _cleanup(self):
+        try:
+            self.loop.run_until_complete(self._logout())
+
+            pending = asyncio.Task.all_tasks(self.loop)
+            gathered = asyncio.gather(*pending, self.loop)
+
+            gathered.cancel()
+            self.loop.run_until_complete(gathered)
+            gathered.exception()
+        except:
+            pass
+
+    async def _logout(self):
+        await self._disconnect_voice_clients()
+        await super().logout()
+
+    async def _disconnect_voice_clients(self):
+        for vc in self.voice_clients:
+            await vc.disconnect()
+
+    @staticmethod
+    def _load_plugins():
+        # Add plugin package to where the system looks for files.
         sys.path.append("plugins")
 
+        plugins = {}
+
+        # TODO CLEANUP?
+
+        # Go through every file in the plugins folder
         for plugin in os.listdir("plugins"):
-            if not plugin == "example.py" and plugin.endswith(".py"):
+            if plugin.endswith(".py") and not plugin == "example.py":
                 plugin_name = plugin.split(".")[0].lower()
+                # Import the plugin module
                 prog = __import__(plugin_name)
+
+                # If the plugin wants to be known by other names, use those names instead.
                 if hasattr(prog, "ALIASES") and len(prog.ALIASES) > 0:
                     for alias in prog.ALIASES:
-                        self.plugins[alias.lower()] = prog
+                        norm_alias = alias.lower()
+
+                        if norm_alias in plugins:
+                            raise CommandConflictError(plugins[norm_alias], norm_alias)
+
+                        plugins[norm_alias] = prog
                 else:
-                    self.plugins[plugin_name] = prog
+                    if plugin_name in plugins:
+                        raise CommandConflictError(plugins[plugin_name], plugin_name)
+                    plugins[plugin_name] = prog
+
+        sys.path.remove("plugins")
+
+        return plugins
 
     @staticmethod
     def _parse_command(message: str, prefix: str):
-        # Grab command and any arguments given
-
-        first_space_idx = message.find(" ")
-
-        if first_space_idx < 0:
-            command = message[len(prefix):len(message)]
-            arguments = ""
+        if message is not None and prefix is not None and message.startswith(prefix):
+            tokens = message.split()
+            command = (tokens[0])[len(prefix):len(tokens[0])]  # Strip the prefix off the command
+            arguments = tokens[1:len(tokens)]
         else:
-            command = message[len(prefix):first_space_idx]
-            arguments = message[first_space_idx + 1:len(message)]
+            command = ""
+            arguments = []
 
         return command, arguments
 
