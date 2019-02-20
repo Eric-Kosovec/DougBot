ce0015769cb7655a0c7c41a33ed74501c52ac92c
Eric-Kosovec <ej.kosovec@gmail.com>
2019-02-20 00:48:01 -0600

    Force update command

--- dougbot/extensions/delivery/delivery.py
+++ dougbot/extensions/delivery/delivery.py
@@ -37,6 +37,28 @@ class ContinuousDeliverySystem:
         finally:
             os.chdir(cwd)
 
+    @commands.command(pass_context=True)
+    async def force_update(self, ctx):
+        if ctx is None:
+            return
+
+        cwd = os.getcwd()
+        os.chdir(self.bot.ROOT_DIR)
+        try:
+            subprocess.check_call(['git', 'fetch', '--all'])
+            subprocess.check_call(['git', 'reset', '--hard', 'origin/master'])
+
+            # Restart ourself
+            pid = os.getpid()
+            print(pid)
+
+            p = subprocess.Popen(['reset.bat', str(pid)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            p.wait()
+        except subprocess.CalledProcessError:
+            await self.bot.confusion(ctx.message)
+        finally:
+            os.chdir(cwd)
+
 
 def setup(bot):
     bot.add_cog(ContinuousDeliverySystem(bot))
