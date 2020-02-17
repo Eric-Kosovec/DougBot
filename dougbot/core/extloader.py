db3bbbfe802c4595a751d1271e7295b77ebda151
Eric Kosovec <Eric-Kosovec@users.noreply.github.com>
2020-02-17 00:28:34 -0600

    Testing

--- dougbot/core/extloader.py
+++ dougbot/core/extloader.py
@@ -5,6 +5,7 @@ import sys
 class ExtensionLoader:
     _extensions_base = os.path.dirname(os.path.dirname(__file__))
     _extensions_base = os.path.join(_extensions_base, 'extensions')
+    TEST_VAR = 1
 
     @classmethod
     def load_extensions(cls, client):
