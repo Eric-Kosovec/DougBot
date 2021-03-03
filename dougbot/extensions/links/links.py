f3f9a22c745b74e2a9b401e140a225fd7f967c34
DimKaz <DimKaz@users.noreply.github.com>
2021-03-03 04:28:46 -0500

    Changing d# to d!
    I think this was done in error
    
    Also removing a space

--- dougbot/extensions/links/links.py
+++ dougbot/extensions/links/links.py
@@ -13,7 +13,7 @@ class Links(commands.Cog):
     @commands.command()
     async def git(self, ctx):
         await ctx.send('https://github.com/Eric-Kosovec/DougBot')
-        
+
     @commands.command()
     async def bingo(self, ctx):
         await ctx.send('http://saddoug.rf.gd/')
