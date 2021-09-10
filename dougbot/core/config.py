7f47cba7b7c3b59e5e40e22dd7e5c10e3fdea6d5
Eric-Kosovec <ej.kosovec@gmail.com>
2021-09-09 22:06:31 -0500

    Separated bot and server config

--- dougbot/core/config.py
+++ dougbot/core/config.py
@@ -3,16 +3,20 @@ from configparser import ConfigParser
 
 class Config:
 
-    def __init__(self, token_file, config_file):
-        self._config_file = config_file
+    def __init__(self, token_file, bot_config, server_config):
+        self._bot_config = bot_config
+        self._server_config = server_config
 
-        config_parser = ConfigParser()
-        config_parser.read(self._config_file)
+        bot_config_parser = ConfigParser()
+        bot_config_parser.read(self._bot_config)
+
+        server_config_parser = ConfigParser()
+        server_config_parser.read(self._server_config)
 
         self.token = self._read_token(token_file)
-        self.command_prefix = config_parser.get('Meta', 'CommandPrefix')
-        self.admin_role_id = int(config_parser.get('Permissions', 'AdminRoleID'))
-        self.logging_channel_id = int(config_parser.get('Channels', 'LoggingChannelID'))
+        self.command_prefix = bot_config_parser.get('Commands', 'Prefix')
+        self.admin_role_id = int(server_config_parser.get('Permissions', 'AdminRoleID'))
+        self.logging_channel_id = int(server_config_parser.get('Channels', 'LoggingChannelID'))
 
     @staticmethod
     def _read_token(token_file):
