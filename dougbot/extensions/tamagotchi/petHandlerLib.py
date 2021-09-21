b401336fb99cc5d0c8f5d512467bc6d6521d2e5e
Matthew Haley <mdhaley@mtu.edu>
2021-09-21 00:43:52 -0400

    Adding basic pet care

--- dougbot/extensions/tamagotchi/petHandlerLib.py
+++ dougbot/extensions/tamagotchi/petHandlerLib.py
@@ -1,4 +1,5 @@
 import json
+import math
 import os
 import datetime
 
@@ -23,7 +24,9 @@ class PetHandler:
         with open(thefile) as json_file:
             json_object = json.load(json_file)
             json_file.close()
-        return json_object
+
+        pet = json_object['tama'][0]
+        return pet
 
     @staticmethod
     def increasecurrenthealth(json_object, amount):
@@ -53,7 +56,7 @@ class PetHandler:
             json_object['food'] = 100
         else:
             json_object['food'] = currentamount + amount
-
+        json_object['lastfeed'] = datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S')
         return json_object
 
     @staticmethod
@@ -73,7 +76,7 @@ class PetHandler:
             json_object['water'] = 100
         else:
             json_object['water'] = currentamount + amount
-
+        json_object['lastwatered'] = datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S')
         return json_object
 
     @staticmethod
@@ -93,7 +96,7 @@ class PetHandler:
             json_object['cleanliness'] = 100
         else:
             json_object['cleanliness'] = currentamount + amount
-
+        json_object['lastcleaned'] = datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S')
         return json_object
 
     @staticmethod
@@ -107,6 +110,26 @@ class PetHandler:
         return json_object
 
     @staticmethod
+    def happy(json_object, amount):
+        currentamount = json_object['happiness']
+        if 100 < (currentamount + amount):
+            json_object['happiness'] = 100
+        else:
+            json_object['happiness'] = currentamount + amount
+        json_object['lastpet'] = datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S')
+        return json_object
+
+    @staticmethod
+    def sadness(json_object, amount):
+        currentamount = json_object['happiness']
+        if 0 > (currentamount - amount):
+            json_object['happiness'] = 0
+        else:
+            json_object['happiness'] = currentamount - amount
+
+        return json_object
+
+    @staticmethod
     def isdead(json_object):
         if json_object['currenthealth'] == 0:
             return True
@@ -116,28 +139,49 @@ class PetHandler:
     @staticmethod
     def death(json_object, reason):
         json_object['deathreason'] = reason
-        json_object['deathdate'] = datetime.datetime.now().strftime('%d/%m/%y %H:%M:%S')
+        json_object['deathdate'] = datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S')
 
         return json_object
 
     @staticmethod
     def checkpet(json_object):
         currentdate = datetime.datetime.now()
-        lastdate = datetime.datetime.strptime(json_object['lastchecked'], '%d/%m/%y %H:%M:%S')
-        delta = currentdate - lastdate
-        dayspassed = delta.days
-        if dayspassed > 0:
-            food = json_object['food']
-            water = json_object['water']
-            cleanliness = json_object['cleanliness']
+        lastdate = datetime.datetime.strptime(json_object['lastchecked'], '%m/%d/%y %H:%M:%S')
+        foodlastdate = datetime.datetime.strptime(json_object['lastfeed'], '%m/%d/%y %H:%M:%S')
+        waterlastdate = datetime.datetime.strptime(json_object['lastwatered'], '%m/%d/%y %H:%M:%S')
+        cleanlastdate = datetime.datetime.strptime(json_object['lastcleaned'], '%m/%d/%y %H:%M:%S')
 
-            if food == 0:
-                json_object = PetHandler.decreasecurrenthealth(json_object, 10 * dayspassed)
-            if water == 0:
-                json_object = PetHandler.decreasecurrenthealth(json_object, 10 * dayspassed)
-            if cleanliness == 0:
-                json_object = PetHandler.decreasecurrenthealth(json_object, 10 * dayspassed)
-        print(json_object)
+        delta = currentdate - lastdate
+        fooddelta = currentdate - foodlastdate
+        waterdelta = currentdate - waterlastdate
+        cleandelta = currentdate - cleanlastdate
+        dayspassed = delta.days
+
+        hourspassed = math.floor((delta.days * 24) + (delta.seconds/3600))
+        foodhourspassed = math.floor((fooddelta.days * 24) + (fooddelta.seconds/3600))
+        waterhourspassed = math.floor((waterdelta.days * 24) + (waterdelta.seconds/3600))
+        cleanhourspassed = math.floor((cleandelta.days * 24) + (cleandelta.seconds/3600))
+
+
+        if foodhourspassed > 0:
+            json_object = PetHandler.starve(json_object, (hourspassed * 2))
+            if json_object['food'] == 0:
+                json_object = PetHandler.decreasecurrenthealth(json_object, 10)
+        if waterhourspassed > 0:
+            json_object = PetHandler.dehydrate(json_object, (hourspassed * 2))
+            if json_object['water'] == 0:
+                json_object = PetHandler.decreasecurrenthealth(json_object, 10)
+        if cleanhourspassed > 0:
+            json_object = PetHandler.dirty(json_object, (hourspassed * 2))
+            if json_object['cleanliness'] == 0:
+                json_object = PetHandler.decreasecurrenthealth(json_object, 10)
+
+        if hourspassed > 0:
+            if json_object['food'] > 70 and json_object['water'] > 75 and json_object['cleanliness'] > 60:
+                json_object = PetHandler.increasecurrenthealth(json_object, (hourspassed * 5))
+
+        json_object['lastchecked'] = currentdate.strftime('%m/%d/%y %H:%M:%S')
+
         return json_object
 
     @staticmethod
@@ -145,22 +189,28 @@ class PetHandler:
 
         json_object = {
             'name': name,
-            'lastchecked': datetime.datetime.now().strftime('%d/%m/%y %H:%M:%S'),
-            'type': 'SadBoy',
+            'lastchecked': datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S'),
+            'type': 'bird',
             'level': 1,
-            'birthdate': datetime.datetime.now().strftime('%d/%m/%y %H:%M:%S'),
+            'birthdate': datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S'),
             'deathdate': '',
             'deathreason': '',
             'maxhealth': 100,
             'currenthealth': 50,
             'attack': 1,
             'defence': 1,
+            'happiness': 50,
+            'lastpet': datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S'),
             'food': 50,
+            'lastfeed': datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S'),
             'water': 50,
-            'cleanliness': 50
+            'lastwatered': datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S'),
+            'cleanliness': 50,
+            'lastcleaned': datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S')
         }
 
         PetHandler.savedata(json_object)
+        return json_object
 
     @staticmethod
     def puttorest(json_object):
