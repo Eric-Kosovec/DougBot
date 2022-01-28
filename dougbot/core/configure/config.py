cd915b4ad2c737e6be8efde7a0f99dc66f3d3b2d
Eric-Kosovec <ej.kosovec@gmail.com>
2022-01-27 22:11:58 -0600

    Reluctantly allow token in test_config.ini as well

--- dougbot/core/configure/config.py
+++ dougbot/core/configure/config.py
@@ -11,4 +11,10 @@ class Config:
         self.command_prefix = config_parser.get('Commands', 'prefix')
         self.admin_role_id = int(config_parser.get('Permissions', 'admin_role_id'))
         self.logging_channel_id = int(config_parser.get('Channels', 'logging_channel_id'))
-        self.token = os.getenv(config_parser.get('Environment', 'token_variable_name'))
+
+        token = os.getenv(config_parser.get('Environment', 'token_variable_name'))
+        if token is None and os.path.exists(test_config):
+            test_config_parser = ConfigParser()
+            test_config_parser.read(test_config)
+            token = config_parser.get('Environment', 'token', fallback=None)
+        self.token = token
