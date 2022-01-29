6b81a2f1241d5a74ae08536b0129e485d65db465
Eric-Kosovec <ej.kosovec@gmail.com>
2022-01-29 16:59:46 -0600

    Example files updates

--- dougbot/extensions/example/example.py
+++ dougbot/extensions/example/example.py
@@ -1,8 +1,6 @@
 from nextcord.ext import commands
 
 from dougbot.core.bot import DougBot
-from dougbot.extensions.common.annotations.admincheck import admin_command
-from dougbot.extensions.common.annotations.miccheck import voice_command
 
 
 class Example(commands.Cog):
@@ -10,19 +8,14 @@ class Example(commands.Cog):
     def __init__(self, bot: DougBot):
         self.bot = bot
 
-    # Some examples of method decorators that can be used.
-    @commands.command()  # Defines a command, using the method name as the command name. (Arguments can be supplied to this: see more in the DiscordPy API docs.)
-    @commands.guild_only()  # Prevents command from being used in private messages.
-    @admin_command()  # Specifies that the command can only be used by people with the admin role.
-    @voice_command()  # Specifies command can only be used while user is in a voice channel.
+    @commands.command()
     async def example_command(self, ctx):
         pass
 
 
-def setup(bot):
+def setup(bot: DougBot):
     bot.add_cog(Example(bot))
 
 
-# Cleanup after extension is unloaded
-def teardown(bot):
+def teardown(bot: DougBot):
     _ = bot
--- dougbot/extensions/example/kvexample.py
+++ dougbot/extensions/example/kvexample.py
@@ -1,12 +1,15 @@
 from nextcord.ext import commands
 
+from dougbot.core.bot import DougBot
+
 
 class KVExample(commands.Cog):
 
-    def __init__(self, bot):
+    def __init__(self, bot: DougBot):
         self.bot = bot
         self.kv = self.bot.kv_store()
 
+    @commands.command()
     async def random_command(self, _):
         self.kv['THIS MUST BE A STRING'] = 'THIS CAN BE ANYTHING, NOT NECESSARY A STRING'
         self.kv['TEST'] = 5
@@ -14,5 +17,10 @@ class KVExample(commands.Cog):
         _ = x
 
 
-def setup(bot):
+def setup(bot: DougBot):
     bot.add_cog(KVExample(bot))
+
+
+def teardown(bot: DougBot):
+    _ = bot
+
