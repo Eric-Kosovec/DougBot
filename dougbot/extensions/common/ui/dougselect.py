8c90bbd011885e439d0644dd3f41f1c7f0e4a1ac
Eric-Kosovec <ej.kosovec@gmail.com>
2022-02-01 00:57:46 -0600

    UI elements

---
+++ dougbot/extensions/common/ui/dougselect.py
@@ -0,0 +1,13 @@
+from nextcord import Interaction
+from nextcord.ui.select import Select
+
+
+class DougSelect(Select):
+
+    def __init__(self, *, callback=None, **kwargs):
+        super().__init__(**kwargs)
+        self._callback = callback
+
+    async def callback(self, interaction: Interaction):
+        if self._callback is not None:
+            await self._callback(interaction)
--- dougbot/extensions/music2/soundplayer2.py
+++ dougbot/extensions/music2/soundplayer2.py
@@ -7,8 +7,11 @@ from collections import defaultdict
 from concurrent.futures import ThreadPoolExecutor
 
 import youtube_dl
+from nextcord import Interaction
 from nextcord.embeds import Embed
 from nextcord.ext import commands
+from nextcord.ui.button import ButtonStyle
+from nextcord.ui.view import View
 
 from dougbot.common import reactions
 from dougbot.common.cache import LRUCache
@@ -16,6 +19,7 @@ from dougbot.core.bot import DougBot
 from dougbot.extensions.common import fileutils
 from dougbot.extensions.common import webutils
 from dougbot.extensions.common.annotations.miccheck import voice_command
+from dougbot.extensions.common.ui.dougbutton import DougButton
 from dougbot.extensions.music2.soundconsumer import SoundConsumer
 from dougbot.extensions.music2.track import Track
 
@@ -24,7 +28,7 @@ class SoundPlayer2(commands.Cog):
 
     _logger = logging.getLogger(__file__)
 
-    def __init__(self, bot: DougBot, sound_consumer: SoundConsumer):
+    def __init__(self, bot: DougBot):
         self.bot = bot
         self.loop = self.bot.loop
 
@@ -39,7 +43,7 @@ class SoundPlayer2(commands.Cog):
         self._clips_dir = os.path.join(self._resource_path, 'audio')
         self._cache_dir = os.path.join(self._resource_path, 'cache')
 
-        self._sound_consumer = sound_consumer
+        self._sound_consumer = SoundConsumer.instance(bot)
         self._sound_consumer_thread = threading.Thread(target=self._sound_consumer.run, name='Sound Consumer', daemon=True)
         self._sound_consumer_thread.start()
 
@@ -48,7 +52,7 @@ class SoundPlayer2(commands.Cog):
     @voice_command()
     async def play2(self, ctx, source: str, *, times: str = '1'):
         source, times = await self._custom_play_parse(source, times)
-        if times <= 0 or times > 30:
+        if times not in range(31):
             await reactions.confusion(ctx.message)
             return
 
@@ -56,15 +60,16 @@ class SoundPlayer2(commands.Cog):
             async with self._order_lock:
                 self._voice = await self.bot.join_voice_channel(ctx.message.author.voice.channel)
 
-        # STOP DOWNLOAD ON STOP?
-        # Skip download on skip?
-
         track = Track(ctx, self._voice, source, times)
         self._sound_consumer.enqueue(track)
         asyncio.create_task(self._prepare_track(track))
 
         await asyncio.sleep(3)
         await ctx.message.delete()
+
+    def cog_unload(self):
+        self._sound_consumer.stop()
+        fileutils.delete_directories(self._cache_dir, True)
 
     async def _prepare_track(self, track):
         if await webutils.async_is_link(track.source):
@@ -101,6 +106,8 @@ class SoundPlayer2(commands.Cog):
         self._file_to_tracks[dl_path].append(track)
 
         ytdl = youtube_dl.YoutubeDL({
+            'format': 'bestaudio/best',
+            'audioformat': 'opus',
             'extractaudio': True,
             'restrictfilenames': True,
             'noplaylist': True,
@@ -122,16 +129,15 @@ class SoundPlayer2(commands.Cog):
         track.thumbnail = info['thumbnails'][-1]['url']
         track.duration = info['duration']
 
-        self._file_to_message[dl_path] = await track.ctx.send(embed=self._link_download_embed(track))
+        self._file_to_message[dl_path] = await track.ctx.send(embed=self._link_download_embed(track), view=self._create_controls_view())
 
         await self.loop.run_in_executor(self._thread_pool, ytdl.extract_info, track.source)
-
         asyncio.create_task(self._delete_embed(dl_path))
 
         return dl_path
 
     async def _delete_embed(self, dl_path):
-        await self._file_to_message[dl_path].delete()
+        # await self._file_to_message[dl_path].delete()
         del self._file_to_message[dl_path]
 
     def _progress_hook(self, data):
@@ -139,7 +145,7 @@ class SoundPlayer2(commands.Cog):
         filename = data['filename']
         embed_message = self._file_to_message[filename]
         embed = self._link_download_embed(self._file_to_tracks[filename][0], progress)
-        asyncio.run_coroutine_threadsafe(embed_message.edit(embed=embed), self.loop)
+        asyncio.run_coroutine_threadsafe(embed_message.edit(embed=embed, view=self._create_controls_view()), self.loop)
 
     @staticmethod
     def _link_download_embed(track, percentage='0.0%'):
@@ -148,9 +154,31 @@ class SoundPlayer2(commands.Cog):
         
             [{track.title}]({track.source})
             '''
-        return Embed(title='Downloading', description=description_markdown, color=0xFF0000) \
-            .set_image(url=track.thumbnail) \
-            .add_field(name='Progress:', value=f'{percentage}' if '100' not in percentage else 'Playing...')
+
+        if '100' in percentage:
+            return Embed(title='Playing', description=description_markdown, color=0xFF0000)\
+                .set_image(url=track.thumbnail)
+        else:
+            return Embed(title='Downloading', description=description_markdown, color=0xFF0000)\
+                .set_image(url=track.thumbnail)\
+                .add_field(name='Progress:', value=f'{percentage}')
+
+    def _create_controls_view(self):
+        view = View(timeout=None)
+        play = DougButton(callback=self._play_button, style=ButtonStyle.primary, label='Play')
+        pause = DougButton(callback=None, style=ButtonStyle.primary, label='Pause')
+        stop = DougButton(callback=None, style=ButtonStyle.danger, label='Stop')
+        next = DougButton(callback=None, style=ButtonStyle.primary, label='Next')
+        view.add_item(play)
+        view.add_item(pause)
+        view.add_item(stop)
+        view.add_item(next)
+        return view
+
+    @staticmethod
+    async def _play_button(interaction: Interaction):
+        print('play pressed')
+        print(interaction)
 
     @staticmethod
     async def _link_hash(link):
@@ -172,10 +200,4 @@ class SoundPlayer2(commands.Cog):
 
 
 def setup(bot):
-    bot.add_cog(SoundPlayer2(bot, SoundConsumer.instance(bot)))
-
-
-def teardown(bot):
-    SoundConsumer.instance(bot).stop()
-    cache_path = os.path.join(bot.extensions_resource_path(), 'music', 'cache')
-    fileutils.delete_directories(cache_path, True)
+    bot.add_cog(SoundPlayer2(bot))
