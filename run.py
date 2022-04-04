d470e5752cf874a4b33a77f5814ca1be6cc1379a
Eric-Kosovec <ej.kosovec@gmail.com>
2022-04-03 21:26:40 -0500

    Support only Python 3.10+

--- run.py
+++ run.py
@@ -3,9 +3,9 @@ import sys
 from dougbot.core.bot import DougBot
 
 try:
-    assert sys.version_info >= (3, 7)
+    assert sys.version_info >= (3, 10)
 except AssertionError:
-    print('Fatal Error: DougBot supports only Python 3.7+', file=sys.stderr)
+    print('Fatal Error: DougBot supports only Python 3.10+', file=sys.stderr)
     exit(1)
 
 
